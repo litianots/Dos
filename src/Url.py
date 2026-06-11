166fa32994855099e45b24c83e62652644d0063e
litianots <lyranoct832@gmail.com>
2026-06-11 13:17:10 +0700

    Update Url.py

--- src/Url.py
+++ src/Url.py
@@ -17,7 +17,7 @@ def cls():
 cls()
 os.system("python src/logo.py")
 
-target = input(f"{GREEN}Enter Target URL: ")
+target = input(f"{GREEN}Enter Target URL(not https://): ")
 target.replace("http://", "")
 target.replace("https://","")
 target.replace("www.","")
